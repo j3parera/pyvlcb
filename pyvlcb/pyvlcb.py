c416bca71de3002578c9307f02c3e75bd0c689b1
José Parera Bermúdez <jose.parera@upm.es>
2024-04-07 21:53:14 +0200

    Eventos a falta de variables.

--- pyvlcb/pyvlcb.py
+++ pyvlcb/pyvlcb.py
@@ -4,45 +4,57 @@ Main module.
 
 from abc import ABC, abstractmethod
 from ctypes import LittleEndianStructure, Union, c_uint8
+from dataclasses import dataclass
 from enum import Enum
 from json import dump, load
 from os import PathLike
-from typing import Any
+from typing import Any, List
 
 
-class Modes(Enum):
+class Mode(Enum):
     """
-    Modes.
+    Module modes.
     """
 
     # Exclusive modes
-    MODE_UNINITIALISED = 0xFF  # Uninitialised / factory settings
-    MODE_SETUP = 0  # Set up mode
-    MODE_NORMAL = 1  # Normal operation mode
+    UNINITIALISED = 0xFF  # Uninitialised / factory settings
+    SETUP = 0  # Set up mode
+    NORMAL = 1  # Normal operation mode
     # Event Teaching Service modes
-    MODE_LEARN_ON = 0x08  # Turn on learn mode
-    MODE_LEARN_OFF = 0x09  # Turn off learn mode
+    LEARN_ON = 0x08  # Turn on learn mode
+    LEARN_OFF = 0x09  # Turn off learn mode
     # Event Acknowledgment Service modes
-    MODE_EVENT_ACK_ON = 0x0A  # Turn on event acknowledgements
-    MODE_EVENT_ACK_OFF = 0x0B  # Turn off event acknowledgements
+    EVENT_ACK_ON = 0x0A  # Turn on event acknowledgements
+    EVENT_ACK_OFF = 0x0B  # Turn off event acknowledgements
     # Minimum Node Service modes
-    MODE_HEARTBEAT_ON = 0x0C  # Turn on heartbeat
-    MODE_HEARTBEAT_OFF = 0x0D  # Turn off heartbeat
+    HEARTBEAT_ON = 0x0C  # Turn on heartbeat
+    HEARTBEAT_OFF = 0x0D  # Turn off heartbeat
     # Boot modes
-    MODE_BOOT = 0x0E  # PIC Boot loader mode
+    BOOT = 0x0E  # PIC Boot loader mode
 
 
 class NodeFlagsBits(LittleEndianStructure):
     """Node flags bits."""
 
+    # pylint: disable=R0903
     _fields_ = [("heartbeat", c_uint8, 1), ("event_ack", c_uint8, 1)]
 
 
 class NodeFlags(Union):
     """Node flags."""
 
+    # pylint: disable=R0903
     _anonymous_ = ("bit",)
     _fields_ = [("bit", NodeFlagsBits), ("as_byte", c_uint8)]
+
+
+@dataclass
+class Event:
+    """VLCB event."""
+
+    node_number: int
+    event_number: int
+    event_vars: List[int]
 
 
 class Configuration(ABC):
@@ -51,170 +63,169 @@ class Configuration(ABC):
     """
 
     def __init__(self, filename: str | PathLike) -> None:
-        self._nv_filename: str | PathLike = filename
-        self._nv_mem: dict[str, dict[str, Any]]
-        # self.node_flags: NodeFlags
+        self._nonvolatile_filename: str | PathLike = filename
+        self._nonvolatile_mem: dict[str, dict[str, Any]] = self._load_nonvolatile_mem()
+        if self.mode == Mode.UNINITIALISED and self.node_number == 0xFFFF:
+            # factory virgin state
+            self.clear_all_node_vars()
+            self.clear_all_events()
+            self.set_mode(Mode.UNINITIALISED)
+            self.can_id = 0
+            # pylint: disable=W0201
+            self.node_flags.as_byte = 0
 
-    def _load_nvmem(self) -> dict[str, dict[str, Any]]:
-        with open(self._nv_filename, "rt", encoding="utf-8") as file:
+    def _load_nonvolatile_mem(self) -> dict[str, dict[str, Any]]:
+        with open(self._nonvolatile_filename, "rt", encoding="utf-8") as file:
             return load(file)
 
-    def _save_nvmem(self) -> None:
-        with open(self._nv_filename, "wt", encoding="utf-8") as file:
-            dump(self._nv_mem, file, indent=4)
+    def _save_nonvolatile_mem(self) -> None:
+        with open(self._nonvolatile_filename, "wt", encoding="utf-8") as file:
+            dump(self._nonvolatile_mem, file, indent=4)
 
     @property
-    def current_mode(self) -> Modes:
+    def mode(self) -> Mode:
         """Current operating mode."""
-        return Modes(self._nv_mem["settings"]["MODE"])
+        return Mode(self._nonvolatile_mem["settings"]["MODE"])
 
-    @current_mode.setter
-    def current_mode(self, mode: Modes) -> None:
-        self._nv_mem["settings"]["MODE"] = mode.value
-        self._save_nvmem()
+    def set_mode(self, mode: Mode, node_number: int = 0) -> None:
+        """Set module mode."""
+        self._nonvolatile_mem["settings"]["MODE"] = mode.value
+        if mode == Mode.UNINITIALISED:
+            self.node_number = 0
+        elif mode == Mode.NORMAL:
+            self.node_number = node_number
+        self._save_nonvolatile_mem()
 
     @property
     def can_id(self) -> int:
         """CAN identifier."""
-        return self._nv_mem["settings"]["CAN_ID"]
+        return self._nonvolatile_mem["settings"]["CAN_ID"]
 
     @can_id.setter
     def can_id(self, can_id: int):
-        self._nv_mem["settings"]["CAN_ID"] = can_id
-        self._save_nvmem()
+        self._nonvolatile_mem["settings"]["CAN_ID"] = can_id
+        self._save_nonvolatile_mem()
 
     @property
     def node_number(self) -> int:
         """Node number."""
-        return self._nv_mem["settings"]["NODE_NUMBER"]
+        return self._nonvolatile_mem["settings"]["NODE_NUMBER"]
 
     @node_number.setter
     def node_number(self, node_number: int):
-        self._nv_mem["settings"]["NODE_NUMBER"] = node_number
-        self._save_nvmem()
+        self._nonvolatile_mem["settings"]["NODE_NUMBER"] = node_number
+        self._save_nonvolatile_mem()
 
     @property
     def node_flags(self) -> NodeFlags:
         """Node flags."""
         flags = NodeFlags()
         # pylint: disable=W0201
-        flags.as_byte = self._nv_mem["settings"]["NODE_FLAGS"]
+        flags.as_byte = self._nonvolatile_mem["settings"]["NODE_FLAGS"]
         return flags
 
     @node_flags.setter
     def node_flags(self, flags: NodeFlags):
-        self._nv_mem["settings"]["NODE_FLAGS"] = flags.value
-        self._save_nvmem()
+        self._nonvolatile_mem["settings"]["NODE_FLAGS"] = flags.value
+        self._save_nonvolatile_mem()
 
     @property
-    def is_heartbeat(self) -> bool:
-        """Is heartbeat active."""
+    def heartbeat(self) -> bool:
+        """Heartbeat."""
         return bool(self.node_flags.heartbeat)
 
+    @heartbeat.setter
+    def heartbeat(self, on: bool) -> None:
+        # pylint: disable=C0103
+        """Heartbeat."""
+        self.node_flags.heartbeat = int(on)
+
     @property
-    def is_event_ack(self) -> bool:
-        """Is event ackkonwledge active."""
+    def event_ack(self) -> bool:
+        """Event acknolwdege."""
         return bool(self.node_flags.event_ack)
 
-    def clear_events(self):
+    @event_ack.setter
+    def event_ack(self, on: bool) -> None:
+        # pylint: disable=C0103
+        self.node_flags.heartbeat = int(on)
+
+    def _event_key(self, idx) -> str:
+        # pylint: disable=R0201
+        return f"{idx:03d}"
+
+    @property
+    def num_events(self) -> int:
+        """Number of events."""
+        return len(self._nonvolatile_mem["events"])
+
+    def read_event(self, idx) -> Event | None:
+        """Read an stored event."""
+        try:
+            evt_key = self._event_key(idx)
+            data = self._nonvolatile_mem["events"][evt_key]
+            evt = Event(data["NODE_NUMBER"], data["EVENT_NUMBER"], data["EVENT_VARS"])
+            return evt
+        except KeyError:
+            return None
+
+    def write_event(self, idx, event: Event) -> None:
+        """Write an event."""
+        evt_key = self._event_key(idx)
+        if evt_key not in self._nonvolatile_mem["events"]:
+            self._nonvolatile_mem["events"][evt_key] = {}
+        self._nonvolatile_mem["events"][evt_key]["NODE_NUMBER"] = event.node_number
+        self._nonvolatile_mem["events"][evt_key]["EVENT_NUMBER"] = event.event_number
+        self._nonvolatile_mem["events"][evt_key]["EVENT_VARS"] = event.event_vars
+        self._save_nonvolatile_mem()
+
+    def clear_event(self, idx) -> None:
+        """Clear an event."""
+        evt_key = self._event_key(idx)
+        try:
+            del self._nonvolatile_mem["events"][evt_key]
+            self._save_nonvolatile_mem()
+        except KeyError:
+            pass
+
+    def find_event(self, node_number: int, event_number: int) -> Event | None:
+        """Locate an event by contents."""
+        for evt in self._nonvolatile_mem["events"].values():
+            if (
+                evt["NODE_NUMBER"] == node_number
+                and evt["EVENT_NUMBER"] == event_number
+            ):
+                return Event(evt["NODE_NUMBER"], evt["EVENT_NUMBER"], evt["EVENT_VARS"])
+        return None
+
+    def clear_all_events(self):
         """Clear all event data."""
-        self._nv_mem["events"] = {}
-        self._save_nvmem()
+        self._nonvolatile_mem["events"] = {}
+        self._save_nonvolatile_mem()
 
-    def clear_node_vars(self):
+    def clear_all_node_vars(self):
         """Clear all node vars data."""
-        self._nv_mem["node_vars"] = {}
-        self._save_nvmem()
+        self._nonvolatile_mem["node_vars"] = {}
+        self._save_nonvolatile_mem()
 
-    def begin(self) -> None:
-        """
-        Do any setup required at the beginning.
-        """
-        self._nv_mem = self._load_nvmem()
-        if self.current_mode == Modes.MODE_UNINITIALISED and self.node_number == 0xFFFF:
-            # factory virgin state
-            self.clear_node_vars()
-            self.clear_events()
-            self.current_mode = Modes.MODE_UNINITIALISED
-            self.can_id = 0
-            self.node_number = 0
-            # pylint: disable=W0201
-            self.node_flags.as_byte = 0
-
-
-#  class Configuration
-#  {
-# public:
-
-#   void begin();
 
 #   byte findExistingEvent(unsigned int nn, unsigned int en);
-#   byte findEventSpace();
 #   byte findExistingEventByEv(byte evnum, byte evval);
 
-#   void printEvHashTable(bool raw);
-#   byte getEvTableEntry(byte tindex);
-#   byte numEvents();
-#   void updateEvHashEntry(byte idx);
-#   void clearEvHashTable();
 #   byte getEventEVval(byte idx, byte evnum);
 #   void writeEventEV(byte idx, byte evnum, byte evval);
+
 
 #   byte readNV(byte idx);
 #   void writeNV(byte idx, byte val);
 
-#   void readEvent(byte idx, byte tarr[EE_HASH_BYTES]);
-#   void writeEvent(byte index, const byte data[EE_HASH_BYTES]);
-#   void cleareventEEPROM(byte index);
-#   void resetModule();
-
-#   void setCANID(byte canid);
 #   void setModuleUninitializedMode();
 #   void setModuleNormalMode(unsigned int nodeNumber);
-#   void setHeartbeat(bool beat);
-#   void setNodeNum(unsigned int nn);
-#   void setEventAck(bool ea);
 
-#   void setResetFlag();
-#   void clearResetFlag();
-#   bool isResetFlagSet();
-
-#   unsigned int EE_EVENTS_START;
-#   byte EE_MAX_EVENTS;
-#   byte EE_NUM_EVS;
-#   byte EE_BYTES_PER_EVENT;
-#   unsigned int EE_NVS_START;
-#   byte EE_NUM_NVS;
-#   byte EE_PRODUCED_EVENTS;
-
-#   bool heartbeat;
-#   bool eventAck;
-#   byte CANID;
-#   VlcbModeParams currentMode;
-#   unsigned int nodeNum;
-
-#   // These functions shouldn't be here. But keep them for now.
-#   unsigned int freeSRAM();
-#   void reboot();
-
-#   static void setTwoBytes(byte *target, unsigned int value);
-#   static unsigned int getTwoBytes(const byte *bytes);
 #   static bool nnenEquals(const byte lhs[EE_HASH_BYTES], const byte rhs[EE_HASH_BYTES]);
 
-# private:
-#   Storage * storage;
 
 #   void setModuleMode(VlcbModeParams m);
-#   byte makeHash(byte tarr[EE_HASH_BYTES]);
-#   void getEvArray(byte idx);
-#   void makeEvHashTable();
-
-#   void loadNVs();
-
-#   unsigned int getEVAddress(byte idx, byte evnum);
-
-#   byte *evhashtbl;
-# };
 
 
 class Service(ABC):
--- tests/test_pyvlcb.py
+++ tests/test_pyvlcb.py
@@ -24,16 +24,64 @@ class TestConfiguration:
 
     def test_factory(self):
         """
-        Begin test
+        Factory settings test
         """
+        # pylint: disable=R0201
         filename = "pyvlcb/config_factory.json"
-        with open(filename, "rt", encoding="utf-8") as f:
-            data = load(f)
+        with open(filename, "rt", encoding="utf-8") as file:
+            data = load(file)
         data["settings"]["MODE"] = 0xFF
         data["settings"]["NODE_NUMBER"] = 0xFFFF
-        with open(filename, "wt", encoding="utf-8") as f:
-            dump(data, f)
+        with open(filename, "wt", encoding="utf-8") as file:
+            dump(data, file)
         config = vlcb.Configuration(filename)
-        config.begin()
-        assert config.current_mode == vlcb.Modes.MODE_UNINITIALISED
+        assert config.mode == vlcb.Mode.UNINITIALISED
         assert config.node_number == 0
+        assert config.heartbeat is False
+
+    def test_events(self):
+        """
+        Event configuration handling test.
+        """
+        # pylint: disable=R0201
+        filename = "pyvlcb/config_factory.json"
+        config = vlcb.Configuration(filename)
+        assert config.num_events == 0
+        assert config.find_event(12, 34) is None
+        config.write_event(0, vlcb.Event(100, 200, [10, 20, 30]))
+        assert config.num_events == 1
+        evt = config.read_event(0)
+        assert evt.node_number == 100
+        assert evt.event_number == 200
+        assert evt.event_vars == [10, 20, 30]
+        assert config.num_events == 1
+        config.write_event(2, vlcb.Event(102, 202, [12, 22, 32]))
+        assert config.num_events == 2
+        config.write_event(1, vlcb.Event(101, 201, [11, 21, 31]))
+        assert config.num_events == 3
+        evt = config.find_event(102, 202)
+        assert evt is not None
+        assert evt.node_number == 102
+        assert evt.event_number == 202
+        config.clear_event(2)
+        assert config.num_events == 2
+        assert config.find_event(102, 202) is None
+        config.clear_all_events()
+        assert config.num_events == 0
+        assert config.read_event(3) is None
+        assert config.clear_event(3) is None
+
+    def test_modes(self):
+        """
+        Module modes test.
+        """
+        # pylint: disable=R0201
+        filename = "pyvlcb/config_factory.json"
+        config = vlcb.Configuration(filename)
+        config.node_number = 123
+        config.set_mode(vlcb.Mode.UNINITIALISED)
+        assert config.mode == vlcb.Mode.UNINITIALISED
+        assert config.node_number == 0
+        config.set_mode(vlcb.Mode.NORMAL, node_number=120)
+        assert config.mode == vlcb.Mode.NORMAL
+        assert config.node_number == 120
